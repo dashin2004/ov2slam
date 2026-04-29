3e637715b0fc180ea87ae406e56fbe4ffbdc9ebe
powertrain1 <markoszym2004@gmail.com>
2026-04-29 13:58:27 +0000

    dodalem funckje draw_guiktora rysuje gui oraz stworzylem maina i loopa rysujacego gui moje jak bozia da bedzie dzialac

--- src/map2d_node.cpp
+++ src/map2d_node.cpp
@@ -15,11 +15,45 @@ public:
 			pointcloud_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(
 				"/pointcloud", 10, std::bind(&MapperNode::point_cloud_callback,this, std::placeholders::_1));
 			
-			}
+			
 			pose_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
 				"/vo_pose",10,std::bind(&MapperNode::pose_callback,this,std::placeholders::_1));
 				grid_map.resize(width_map*height_map);
-		}
+			}
+			void draw_gui(){
+				ImGui::Begin("map");
+				//drawing
+				ImDrawList* draw_list = ImGui::GetWindowDrawList();
+				ImVec2 p = ImGui::GetCursorScreenPos();
+				for(int y = 0; y < height_map; y++){
+					for(int x = 0; x < width_map; x++){
+						int index = (y*66) + x;
+						int hits = grid_map[index];
+						ImU32 color;
+						int redness = hits*5;
+
+						if (redness < 255 && redness > 0){
+							color = IM_COL32(redness, 0, 0, 255);
+						}
+						else if(redness == 0){
+							color = IM_COL32(255,255,255,255);
+						}
+						else{
+							color = IM_COL32(255, 0, 0, 255);
+						}
+						draw_list->AddRectFilled(ImVec2(p.x+ (x*5), p.y +(y*5)),
+						ImVec2(p.x + ((x + 1) * 5), p.y + ((y + 1) * 5)),  
+						color );
+					}
+				}
+				int robot_grid_x = static_cast<int>((pose_x + (map_size / 2.0f)) / resolution);
+				int robot_grid_y = static_cast<int>((pose_y + (map_size / 2.0f)) / resolution);
+				draw_list->AddCircleFilled(ImVec2(p.x + robot_grid_x*5,p.y + robot_grid_y*5),
+				3.0f,
+				IM_COL32(0, 255, 0, 255));
+				ImGui::Dummy(ImVec2(width_map * 5, height_map * 5));
+				ImGui::End();
+		
 private:
 		void point_cloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg){
 			pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
@@ -33,7 +67,7 @@ private:
 		}
 		void mapping(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 			for(auto& point : cloud->points){
-				if (point.z > 1 || point.z < 0.1) {
+				if (point.z > 1 || point.z < -0.1) {
 					continue;
 				}
 				
@@ -62,3 +96,44 @@ private:
 		std::vector<int> grid_map;
 		float pose_x;
 		float pose_y;
+	};
+int main(int argc, char**  argv){
+	rclcpp::init(argc,argv);
+	auto node = std::make_shared<MapperNode>();
+	if(!glfwInit()) return -1;
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OV2SLAM MAP", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	while(!glfwWindowShouldClose(window)){
+		glfwPollEvents();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		rclcpp::spin_some(node);
+		node->draw_gui();
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+	}
+	    
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
+    
+    rclcpp::shutdown();
+    return 0;
+
+}
