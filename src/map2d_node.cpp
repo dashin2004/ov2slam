7e8f48e5b91523884a22b73986d8a7cf17e04187
powertrain1 <markoszym2004@gmail.com>
2026-05-06 20:48:42 +0000

    w tej wersji naprawilem tak zeby cokolwiek sie rysowalo na tej mape wczesniej byl blad z nazwa topica oraz ov2slam ma konwecje ze gora dol to y wiec zmienilem tak zeby czytal pozycje z zamiast y

--- src/map2d_node.cpp
+++ src/map2d_node.cpp
@@ -13,7 +13,7 @@ class MapperNode : public rclcpp::Node {
 public:
 	MapperNode() : Node("mapper2d") {
 			pointcloud_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-				"/pointcloud", 10, std::bind(&MapperNode::point_cloud_callback,this, std::placeholders::_1));
+				"/point_cloud", 10, std::bind(&MapperNode::point_cloud_callback,this, std::placeholders::_1));
 			
 			
 			pose_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
@@ -57,23 +57,23 @@ public:
 		
 private:
 		void point_cloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg){
-			pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
-
-			pcl::fromROSMsg(*msg, pcl_cloud);
+			auto pcl_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+			pcl::fromROSMsg(*msg, *pcl_cloud);
+			mapping(pcl_cloud);
 			
 		}
 		void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
 			pose_x = msg->pose.position.x;
-			pose_y = msg->pose.position.y;
+			pose_y = msg->pose.position.z;
 		}
 		void mapping(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 			for(auto& point : cloud->points){
-				if (point.z > 1 || point.z < -0.1) {
+				if (point.y < -0.5 || point.y > 0.5) {
 					continue;
 				}
 				
 				int grid_x = static_cast<int>((point.x + (map_size / 2.0)) / resolution);
-				int grid_y = static_cast<int>((point.y + (map_size / 2.0)) / resolution);
+				int grid_y = static_cast<int>((point.z + (map_size / 2.0)) / resolution);
 				
 				if (grid_x < 0 || grid_x >= width_map || grid_y < 0 || grid_y >= height_map) {
     				continue;
@@ -92,7 +92,7 @@ private:
 		rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub;
 		const int width_map = 66;
 		const int height_map = 66;
-		const float map_size = 2.0; //2m na mape
+		const float map_size = 20.0; //2m na mape
 		const float resolution = map_size/width_map; //3cm na komorke
 		std::vector<int> grid_map;
 		float pose_x;
